b2984ab816fb2e3d1257ba4ae8043b2fbb09123e
Maurizio Tomasi <ziotom78@gmail.com>
2021-03-29 12:45:16 +0200

    Add license text to source files

--- test_all.py
+++ test_all.py
@@ -1,3 +1,20 @@
+# The MIT License (MIT)
+#
+# Copyright © 2021 Maurizio Tomasi
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+# documentation files (the “Software”), to deal in the Software without restriction, including without limitation the
+# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+# and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+# the Software. THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+# LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT
+# SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+# CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
+
+
 import unittest
 from io import BytesIO
 from colors import Color
@@ -140,6 +157,7 @@ class TestHdrImage(unittest.TestCase):
         buf = BytesIO(b"PF\n3 2\n-1.0\nstop")
         with pytest.raises(InvalidPfmFileFormat):
             _ = read_pfm_image(buf)
+
 
 if __name__ == '__main__':
     unittest.main()
