055f855c0f0f50266c6453d68dcf0e37ba0f12c5
Maurizio Tomasi <ziotom78@gmail.com>
2021-04-10 08:36:57 +0200

    Add classes `Vec` and `Point`

---
+++ geometry.py
@@ -0,0 +1,15 @@
+# -*- encoding: utf-8 -*-
+
+from dataclasses import dataclass
+
+@dataclass
+class Vec:
+    x: float = 0.0
+    y: float = 0.0
+    z: float = 0.0
+
+@dataclass
+class Point:
+    x: float = 0.0
+    y: float = 0.0
+    z: float = 0.0
