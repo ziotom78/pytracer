de469e5b01357398ae5d24eb5ac328e9c7a13af0
Maurizio Tomasi <ziotom78@gmail.com>
2021-04-20 18:49:10 +0200

    Implement "Transformation"

--- geometry.py
+++ geometry.py
@@ -4,7 +4,6 @@ import math
 from dataclasses import dataclass
 from colors import are_close
 
-
 def _are_xyz_close(a, b):
     # This works thanks to Python's duck typing. In C++ and other languages
     # you should probably rely on function templates or something like
@@ -23,6 +22,17 @@ def _sub_xyz(a, b, return_type):
 
 def _mul_scalar_xyz(scalar, xyz, return_type):
     return return_type(scalar * xyz.x, scalar * xyz.y, scalar * xyz.z)
+
+
+def _get_xyz_element(self, item):
+    assert (item >= 0) and (item < 3), f"wrong vector index {item}"
+
+    if item == 0:
+        return self.x
+    elif item == 1:
+        return self.y
+
+    return self.z
 
 
 @dataclass
@@ -52,6 +62,12 @@ class Vec:
     def __mul__(self, scalar):
         return _mul_scalar_xyz(scalar=scalar, xyz=self, return_type=Vec)
 
+    def __getitem__(self, item):
+        return _get_xyz_element(self, item)
+
+    def __neg__(self):
+        return Vec(-self.x, -self.y, -self.z)
+
     def dot(self, other):
         return self.x * other.x + self.y * other.y + self.z * other.z
 
@@ -71,6 +87,7 @@ class Vec:
         x /= norm
         y /= norm
         z /= norm
+
 
 @dataclass
 class Point:
@@ -98,3 +115,22 @@ class Point:
 
     def __mul__(self, scalar):
         return _mul_scalar_xyz(scalar=scalar, xyz=self, return_type=Point)
+
+    def __getitem__(self, item):
+        return _get_xyz_element(self, item)
+
+
+@dataclass
+class Normal:
+    x: float = 0.0
+    y: float = 0.0
+    z: float = 0.0
+
+    def is_close(self, other):
+        assert isinstance(other, Normal)
+        return _are_xyz_close(self, other)
+
+
+VEC_X = Vec(1.0, 0.0, 0.0)
+VEC_Y = Vec(0.0, 1.0, 0.0)
+VEC_Z = Vec(0.0, 0.0, 1.0)
--- test_all.py
+++ test_all.py
@@ -14,13 +14,16 @@
 # CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 # IN THE SOFTWARE.
 
+from copy import deepcopy
+from math import pi, sin, cos
 
 import unittest
 from io import BytesIO
 from colors import Color
 from hdrimages import HdrImage, InvalidPfmFileFormat, Endianness, read_pfm_image, _read_line, _parse_img_size, \
     _parse_endianness
-from geometry import Vec, Point
+from geometry import Vec, Point, Normal, VEC_X, VEC_Y, VEC_Z
+from transformations import Transformation, _matr_prod, translation, scaling, rotation_x, rotation_y, rotation_z
 import pytest
 
 
@@ -37,12 +40,12 @@ class TestColor(unittest.TestCase):
         col1 = Color(1.0, 2.0, 3.0)
         col2 = Color(5.0, 7.0, 9.0)
 
-        assert (col1 + col2).is_close(Color(6.0, 9.0, 12.0))
-        assert (col1 - col2).is_close(Color(-4.0, -5.0, -6.0))
-        assert (col1 * col2).is_close(Color(5.0, 14.0, 27.0))
+        assert (col1 + col2)._are_matr_close(Color(6.0, 9.0, 12.0))
+        assert (col1 - col2)._are_matr_close(Color(-4.0, -5.0, -6.0))
+        assert (col1 * col2)._are_matr_close(Color(5.0, 14.0, 27.0))
 
         prod_col = Color(1.0, 2.0, 3.0) * 2.0
-        assert prod_col.is_close(Color(2.0, 4.0, 6.0))
+        assert prod_col._are_matr_close(Color(2.0, 4.0, 6.0))
 
     def test_luminosity(self):
         col1 = Color(1.0, 2.0, 3.0)
@@ -153,13 +156,13 @@ class TestHdrImage(unittest.TestCase):
             assert img.width == 3
             assert img.height == 2
 
-            assert img.get_pixel(0, 0).is_close(Color(1.0e1, 2.0e1, 3.0e1))
-            assert img.get_pixel(1, 0).is_close(Color(4.0e1, 5.0e1, 6.0e1))
-            assert img.get_pixel(2, 0).is_close(Color(7.0e1, 8.0e1, 9.0e1))
-            assert img.get_pixel(0, 1).is_close(Color(1.0e2, 2.0e2, 3.0e2))
-            assert img.get_pixel(0, 0).is_close(Color(1.0e1, 2.0e1, 3.0e1))
-            assert img.get_pixel(1, 1).is_close(Color(4.0e2, 5.0e2, 6.0e2))
-            assert img.get_pixel(2, 1).is_close(Color(7.0e2, 8.0e2, 9.0e2))
+            assert img.get_pixel(0, 0)._are_matr_close(Color(1.0e1, 2.0e1, 3.0e1))
+            assert img.get_pixel(1, 0)._are_matr_close(Color(4.0e1, 5.0e1, 6.0e1))
+            assert img.get_pixel(2, 0)._are_matr_close(Color(7.0e1, 8.0e1, 9.0e1))
+            assert img.get_pixel(0, 1)._are_matr_close(Color(1.0e2, 2.0e2, 3.0e2))
+            assert img.get_pixel(0, 0)._are_matr_close(Color(1.0e1, 2.0e1, 3.0e1))
+            assert img.get_pixel(1, 1)._are_matr_close(Color(4.0e2, 5.0e2, 6.0e2))
+            assert img.get_pixel(2, 1)._are_matr_close(Color(7.0e2, 8.0e2, 9.0e2))
 
     def test_pfm_read_wrong(self):
         buf = BytesIO(b"PF\n3 2\n-1.0\nstop")
@@ -182,8 +185,8 @@ class TestHdrImage(unittest.TestCase):
         img.set_pixel(1, 0, Color(0.5e3, 1.0e3, 1.5e3))
 
         img.normalize_image(factor=1000.0, luminosity=100.0)
-        assert img.get_pixel(0, 0).is_close(Color(0.5e2, 1.0e2, 1.5e2))
-        assert img.get_pixel(1, 0).is_close(Color(0.5e4, 1.0e4, 1.5e4))
+        assert img.get_pixel(0, 0)._are_matr_close(Color(0.5e2, 1.0e2, 1.5e2))
+        assert img.get_pixel(1, 0)._are_matr_close(Color(0.5e4, 1.0e4, 1.5e4))
 
     def test_clamp_image(self):
         img = HdrImage(2, 1)
@@ -209,12 +212,13 @@ class TestGeometry(unittest.TestCase):
     def test_vector_operations(self):
         a = Vec(1.0, 2.0, 3.0)
         b = Vec(4.0, 6.0, 8.0)
-        assert (a + b).is_close(Vec(5.0, 8.0, 11.0))
-        assert (b - a).is_close(Vec(3.0, 4.0, 5.0))
-        assert (a * 2).is_close(Vec(2.0, 4.0, 6.0))
+        assert (-a)._are_matr_close(Vec(-1.0, -2.0, -3.0))
+        assert (a + b)._are_matr_close(Vec(5.0, 8.0, 11.0))
+        assert (b - a)._are_matr_close(Vec(3.0, 4.0, 5.0))
+        assert (a * 2)._are_matr_close(Vec(2.0, 4.0, 6.0))
         assert pytest.approx(40.0) == a.dot(b)
-        assert a.cross(b).is_close(Vec(-2.0, 4.0, -2.0))
-        assert b.cross(a).is_close(Vec(2.0, -4.0, 2.0))
+        assert a.cross(b)._are_matr_close(Vec(-2.0, 4.0, -2.0))
+        assert b.cross(a)._are_matr_close(Vec(2.0, -4.0, 2.0))
         assert pytest.approx(14.0) == a.squared_norm()
         assert pytest.approx(14.0) == a.norm() ** 2
 
@@ -228,10 +232,121 @@ class TestGeometry(unittest.TestCase):
         p1 = Point(1.0, 2.0, 3.0)
         v = Vec(4.0, 6.0, 8.0)
         p2 = Point(4.0, 6.0, 8.0)
-        assert (p1 * 2).is_close(Point(2.0, 4.0, 6.0))
-        assert (p1 + v).is_close(Point(5.0, 8.0, 11.0))
-        assert (p2 - p1).is_close(Vec(3.0, 4.0, 5.0))
-        assert (p1 - v).is_close(Point(-3.0, -4.0, -5.0))
+        assert (p1 * 2)._are_matr_close(Point(2.0, 4.0, 6.0))
+        assert (p1 + v)._are_matr_close(Point(5.0, 8.0, 11.0))
+        assert (p2 - p1)._are_matr_close(Vec(3.0, 4.0, 5.0))
+        assert (p1 - v)._are_matr_close(Point(-3.0, -4.0, -5.0))
+
+
+class TestTransformations(unittest.TestCase):
+    def test_is_close(self):
+        m1 = Transformation(m=[[1.0, 2.0, 3.0, 4.0],
+                               [5.0, 6.0, 7.0, 8.0],
+                               [9.0, 9.0, 8.0, 7.0],
+                               [6.0, 5.0, 4.0, 1.0]],
+                            invm=[[-3.75, 2.75, -1, 0],
+                                  [4.375, -3.875, 2.0, -0.5],
+                                  [0.5, 0.5, -1.0, 1.0],
+                                  [-1.375, 0.875, 0.0, -0.5]])
+
+        assert m1.is_consistent()
+
+        # Not using "deepcopy" here would make Python pass a pointer to the *same* matrices and vectors
+        m2 = Transformation(m=deepcopy(m1.m), invm=deepcopy(m1.invm))
+        assert m1.is_close(m2)
+
+        m3 = Transformation(m=deepcopy(m1.m), invm=deepcopy(m1.invm))
+        m3.m[2][2] += 1.0
+        assert not m1.is_close(m3)
+
+        m4 = Transformation(m=deepcopy(m1.m), invm=deepcopy(m1.invm))
+        m4.invm[2][2] += 1.0
+        assert not m1.is_close(m4)
+
+    def test_multiplication(self):
+        m1 = Transformation(m=[[1.0, 2.0, 3.0, 4.0],
+                               [5.0, 6.0, 7.0, 8.0],
+                               [9.0, 9.0, 8.0, 7.0],
+                               [6.0, 5.0, 4.0, 1.0]],
+                            invm=[[-3.75, 2.75, -1, 0],
+                                  [4.375, -3.875, 2.0, -0.5],
+                                  [0.5, 0.5, -1.0, 1.0],
+                                  [-1.375, 0.875, 0.0, -0.5]])
+        m2 = Transformation(m=[[3.0, 5.0, 2.0, 4.0],
+                               [4.0, 1.0, 0.0, 5.0],
+                               [6.0, 3.0, 2.0, 0.0],
+                               [1.0, 4.0, 2.0, 1.0]],
+                            invm=[[0.4, -0.2, 0.2, -0.6],
+                                  [2.9, -1.7, 0.2, -3.1],
+                                  [-5.55, 3.15, -0.4, 6.45],
+                                  [-0.9, 0.7, -0.2, 1.1]])
+
+        expected = Transformation(
+            m=[[33.0, 32.0, 16.0, 18.0],
+               [89.0, 84.0, 40.0, 58.0],
+               [118.0, 106.0, 48.0, 88.0],
+               [63.0, 51.0, 22.0, 50.0]],
+            invm=[[-1.45, 1.45, -1.0, 0.6],
+                  [-13.95, 11.95, -6.5, 2.6],
+                  [25.525, -22.025, 12.25, -5.2],
+                  [4.825, -4.325, 2.5, -1.1]],
+        )
+
+        assert expected.is_close(m1 * m2)
+
+    def test_vec_point_multiplication(self):
+        m = Transformation(m=[[1.0, 2.0, 3.0, 4.0],
+                              [5.0, 6.0, 7.0, 8.0],
+                              [9.0, 9.0, 8.0, 7.0],
+                              [0.0, 0.0, 0.0, 1.0]],
+                           invm=[[-3.75, 2.75, -1, 0],
+                                 [5.75, -4.75, 2.0, 1.0],
+                                 [-2.25, 2.25, -1.0, -2.0],
+                                 [0.0, 0.0, 0.0, 1.0]])
+
+        expected_v = Vec(14.0, 38.0, 51.0)
+        assert expected_v.is_close(m * Vec(1.0, 2.0, 3.0))
+
+        expected_p = Point(18.0, 46.0, 58.0)
+        assert expected_p.is_close(m * Point(1.0, 2.0, 3.0))
+
+        expected_n = Normal(-8.75, 7.75, -3.0)
+        assert expected_n.is_close(m * Normal(3.0, 2.0, 4.0))
+
+    def test_inverse(self):
+        m1 = Transformation(m=[[1.0, 2.0, 3.0, 4.0],
+                               [5.0, 6.0, 7.0, 8.0],
+                               [9.0, 9.0, 8.0, 7.0],
+                               [6.0, 5.0, 4.0, 1.0]],
+                            invm=[[-3.75, 2.75, -1, 0],
+                                  [4.375, -3.875, 2.0, -0.5],
+                                  [0.5, 0.5, -1.0, 1.0],
+                                  [-1.375, 0.875, 0.0, -0.5]])
+
+        m2 = m1.inverse()
+
+        prod = m1 * m2
+        assert prod.is_close(Transformation())
+
+    def test_translations(self):
+        tr1 = translation(Vec(1.0, 2.0, 3.0))
+        tr2 = translation(Vec(4.0, 6.0, 8.0))
+        prod = tr1 * tr2
+
+        expected = translation(Vec(5.0, 8.0, 11.0))
+        assert prod.is_close(expected)
+
+    def test_rotations(self):
+        assert (rotation_x(angle_rad=pi / 2) * VEC_Y).is_close(VEC_Z)
+        assert (rotation_y(angle_rad=pi / 2) * VEC_Z).is_close(VEC_X)
+        assert (rotation_z(angle_rad=pi / 2) * VEC_X).is_close(VEC_Y)
+
+    def test_scalings(self):
+        tr1 = scaling(Vec(2.0, 5.0, 10.0))
+        tr2 = scaling(Vec(3.0, 2.0, 4.0))
+
+        expected = scaling(Vec(6.0, 10.0, 40.0))
+        assert expected.is_close(tr1 * tr2)
 
 
 if __name__ == '__main__':
