a34b824bc46d90e58c2b4a88704e0ebbeed37799
Maurizio Tomasi <ziotom78@gmail.com>
2021-05-17 12:46:14 +0200

    Add missing import

--- materials.py
+++ materials.py
@@ -16,6 +16,7 @@
 # CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 # IN THE SOFTWARE.
 
+from dataclasses import dataclass
 from math import floor, pi
 
 from colors import Color, BLACK, WHITE
