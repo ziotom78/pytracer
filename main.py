158b5a2574592f94f770e675705447bce351cb40
Maurizio Tomasi <ziotom78@gmail.com>
2021-05-18 19:26:25 +0200

    Update the demo to use different renderers

--- main.py
+++ main.py
@@ -17,6 +17,7 @@
 # IN THE SOFTWARE.
 
 from dataclasses import dataclass
+import sys
 
 from hdrimages import HdrImage, Endianness, read_pfm_image
 from camera import OrthogonalCamera, PerspectiveCamera
@@ -27,6 +28,8 @@ from ray import Ray
 from shapes import Sphere
 from transformations import translation, scaling, rotation_z
 from world import World
+from materials import UniformPigment, CheckeredPigment, ImagePigment, DiffuseBRDF, Material
+from render import OnOffRenderer, FlatRenderer
 
 import click
 
@@ -44,10 +47,14 @@ def cli():
     pass
 
 
+RENDERERS = ["onoff", "flat"]
+
+
 @click.command("demo")
 @click.option("--width", type=int, default=640, help="Width of the image to render")
 @click.option("--height", type=int, default=480, help="Height of the image to render")
 @click.option("--angle-deg", type=float, default=0.0, help="Angle of view")
+@click.option('--algorithm', type=click.Choice(RENDERERS), default="flat")
 @click.option(
     "--pfm-output",
     type=str,
@@ -65,11 +72,27 @@ def cli():
     is_flag=True,
     help="Use an orthogonal camera instead of a perspective camera",
 )
-def demo(width, height, angle_deg, orthogonal, pfm_output, png_output):
-    image = HdrImage(width, height)
-
+def demo(width, height, angle_deg, algorithm, orthogonal, pfm_output, png_output):
     # Create a world and populate it with a few shapes
     world = World()
+
+    material1 = Material(
+        brdf=DiffuseBRDF(UniformPigment(Color(0.7, 0.3, 0.2)))
+    )
+
+    material2 = Material(
+        brdf=DiffuseBRDF(CheckeredPigment(Color(0.2, 0.7, 0.3), Color(0.3, 0.2, 0.7), num_of_steps=4)),
+    )
+
+    sphere_texture = HdrImage(2, 2)
+    sphere_texture.set_pixel(0, 0, Color(0.1, 0.2, 0.3))
+    sphere_texture.set_pixel(0, 1, Color(0.2, 0.1, 0.3))
+    sphere_texture.set_pixel(1, 0, Color(0.3, 0.2, 0.1))
+    sphere_texture.set_pixel(1, 1, Color(0.3, 0.1, 0.2))
+
+    material3 = Material(
+        brdf=DiffuseBRDF(ImagePigment(sphere_texture))
+    )
 
     for x in [-0.5, 0.5]:
         for y in [-0.5, 0.5]:
@@ -77,7 +100,8 @@ def demo(width, height, angle_deg, orthogonal, pfm_output, png_output):
                 world.add(
                     Sphere(
                         transformation=translation(Vec(x, y, z))
-                                       * scaling(Vec(0.1, 0.1, 0.1))
+                                       * scaling(Vec(0.1, 0.1, 0.1)),
+                        material=material1,
                     )
                 )
 
@@ -87,14 +111,19 @@ def demo(width, height, angle_deg, orthogonal, pfm_output, png_output):
     world.add(
         Sphere(
             transformation=translation(Vec(0.0, 0.0, -0.5))
-                           * scaling(Vec(0.1, 0.1, 0.1))
+                           * scaling(Vec(0.1, 0.1, 0.1)),
+            material=material2,
         )
     )
+
     world.add(
         Sphere(
-            transformation=translation(Vec(0.0, 0.5, 0.0)) * scaling(Vec(0.1, 0.1, 0.1))
+            transformation=translation(Vec(0.0, 0.5, 0.0)) * scaling(Vec(0.1, 0.1, 0.1)),
+            material=material3,
         )
     )
+
+    image = HdrImage(width, height)
 
     # Initialize a camera
     camera_tr = rotation_z(angle_deg=angle_deg) * translation(Vec(-1.0, 0.0, 0.0))
@@ -109,13 +138,17 @@ def demo(width, height, angle_deg, orthogonal, pfm_output, png_output):
 
     tracer = ImageTracer(image=image, camera=camera)
 
-    def compute_color(ray: Ray) -> Color:
-        if world.ray_intersection(ray):
-            return WHITE
-        else:
-            return BLACK
+    if algorithm == "onoff":
+        print("Using on/off renderer")
+        renderer = OnOffRenderer(world=world, background_color=BLACK)
+    elif algorithm == "flat":
+        print("Using flat renderer")
+        renderer = FlatRenderer(world=world, background_color=BLACK)
+    else:
+        print(f"Unknown renderer: {algorithm}")
+        sys.exit(1)
 
-    tracer.fire_all_rays(compute_color)
+    tracer.fire_all_rays(renderer)
 
     # Save the HDR image
     with open(pfm_output, "wb") as outf:
@@ -123,7 +156,7 @@ def demo(width, height, angle_deg, orthogonal, pfm_output, png_output):
     print(f"HDR demo image written to {pfm_output}")
 
     # Apply tone-mapping to the image
-    image.normalize_image(factor=1.0)
+    image.normalize_image(factor=0.3)
     image.clamp_image()
 
     # Save the LDR image
@@ -133,12 +166,10 @@ def demo(width, height, angle_deg, orthogonal, pfm_output, png_output):
 
 
 @click.command("pfm2png")
-@click.option("--factor", type=float, default=0.2, help="Multiplicative factor")
-@click.option(
-    "--gamma", type=float, default=1.0, help="Value to be used for gamma correction"
-)
-@click.argument("input_pfm_file_name")
-@click.argument("output_png_file_name")
+@click.option("--factor", type=float, default=0.7, help="Multiplicative factor")
+@click.option("--gamma", type=float, default=1.0, help="Exponent for gamma-correction")
+@click.argument("input_pfm_file_name", type=str)
+@click.argument("output_png_file_name", type=str)
 def pfm2png(factor, gamma, input_pfm_file_name, output_png_file_name):
     with open(input_pfm_file_name, "rb") as inpf:
         img = read_pfm_image(inpf)
