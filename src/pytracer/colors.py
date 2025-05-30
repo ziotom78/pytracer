6c750853a27f64582c065b55c3b43281e348e9ff
Maurizio Tomasi <ziotom78@gmail.com>
2025-05-30 12:58:28 +0200

    Add a test for the point-light tracer that fails because of issue #19

--- src/pytracer/colors.py
+++ src/pytracer/colors.py
@@ -71,3 +71,9 @@ class Color:
 
 BLACK = Color(0.0, 0.0, 0.0)
 WHITE = Color(1.0, 1.0, 1.0)
+RED = Color(1.0, 0.0, 0.0)
+GREEN = Color(0.0, 1.0, 0.0)
+BLUE = Color(0.0, 0.0, 1.0)
+YELLOW = Color(1.0, 1.0, 0.0)
+PURPLE = Color(1.0, 0.0, 1.0)
+CYAN = Color(0.0, 1.0, 1.0)
